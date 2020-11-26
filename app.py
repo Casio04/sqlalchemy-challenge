9333f1866bad41de5c730313ee39871039a14483
Casio04 <carlos.casio@outlook.com>
2020-11-26 15:37:11 -0600

    Added two routes to the same query and deleted a jupyter notebook used to help me check the queries from the API

--- app.py
+++ app.py
@@ -23,8 +23,11 @@ app = Flask(__name__)
 # Create first route including only the links to the other routes
 @app.route("/")
 def home():
+     # Here I obtain the max and min date to query later for the JSON lists
     max_date = session.query(func.max(Measurement.date)).scalar()
     min_date = session.query(func.min(Measurement.date)).scalar()
+    
+    # The home route only lists the routes available and informs the user about the dates available to query
     return(f"""<section style="font-family:Arial">
     <h1 style="color:darkblue">Hello. This is the API index.</h1>
     <p> The minimum date available is: <b>{min_date}</b>. <br>
@@ -34,19 +37,43 @@ def home():
         <li><a target="_blank" href="/api/v1.0/precipitation">/api/v1.0/precipitation </a> - Daily precipitation data from the last 12 months available</li>
         <li><a target="_blank" href="/api/v1.0/stations">/api/v1.0/stations</a> - The list of available stations as of today</li>
         <li><a target="_blank" href="/api/v1.0/tobs">/api/v1.0/tobs</a> - Daily temperatures observed data from the last 12 months available</li>
-        <li><a target="_blank" href="/api/v1.0/start/end">/api/v1.0/start/end</a> - Returns the maximum, minimum and average temperature for a range of time.</li>
+        <li><a target="_blank" href="/api/v1.0/start/end">/api/v1.0/start/end</a> - Returns the minimum, maximum and average (on that order) temperature for a range of time.</li>
     </ol>
     </section>""")
 
-@app.route("/api/v1.0/precipitation")
-def prec():
-    # Filtering the data from the last available year in the database
-    prec_data = session.query(Measurement).filter(Measurement.date >= "2016-08-23").group_by(Measurement.date).all()
-    prec_dict = {}
-    # Looping through every date available and turning that into a JSON
-    for x in prec_data:
-        prec_dict[f"{x.date}"] = x.prcp
-    return  jsonify(prec_dict)
+@app.route("/api/v1.0/tobs", endpoint="temperatures")
+@app.route("/api/v1.0/precipitation", endpoint="precipitation")
+def prec_temp():
+     # On these queries only the max date is needed to substract one year to it
+    max_date = session.query(func.max(Measurement.date)).scalar()
+    
+    # Here I separate the year, month and day from the max date to substract 1 to the year and then return it to string
+    # Also a zfill function is used to fill a zero number at the beginning to have always two digits. If not done, the query is wrong
+    year = int(max_date[:4]) - 1
+    month = max_date[6:7].zfill(2)
+    day = max_date[-2:].zfill(2)
+
+    # This will be the variable used to filter for the last 12 months available since the maximum date calculated
+    year_before = f"{year}-{month}-{day}"
+
+    if request.endpoint == "precipitation":
+        # Filtering the data from the last available year in the database
+        prec_data = session.query(Measurement).filter(Measurement.date >= year_before).group_by(Measurement.date).all()
+        prec_dict = {}
+        # Looping through every date available and turning that into a JSON
+        for x in prec_data:
+            prec_dict[f"{x.date}"] = x.prcp
+        return  jsonify(prec_dict)
+    else:
+        # Query all the temperatures observed for the last year available
+        active_station = session.query(Measurement.station, func.count(Measurement.station).label("tobs")).group_by(Measurement.station).order_by(desc("tobs")).first()[0]
+        temp_obs = session.query(Measurement).filter(and_(Measurement.station == active_station, Measurement.date >= year_before)).all()
+        temp_list = {}
+
+        #  Insert Date and Temperature Observed as a JSON
+        for temp in temp_obs:
+            temp_list[temp.date] = temp.tobs
+        return jsonify(temp_list)
 
 @app.route("/api/v1.0/stations")
 def stns ():
@@ -58,18 +85,6 @@ def stns ():
     for x in stations:
         station_list.append(x.station)
     return jsonify(station_list)
-
-@app.route("/api/v1.0/tobs")
-def tobs():
-    # Query all the temperatures observed for the last year available
-    active_station = session.query(Measurement.station, func.count(Measurement.station).label("tobs")).group_by(Measurement.station).order_by(desc("tobs")).first()[0]
-    temp_obs = session.query(Measurement).filter(and_(Measurement.station == active_station, Measurement.date >= "2016-08-23")).all()
-    temp_list = {}
-
-    #  Insert Date and Temperature Observed as a JSON
-    for temp in temp_obs:
-        temp_list[temp.date] = temp.tobs
-    return jsonify(temp_list)
 
 #  Creating two routes to query for the same filter
 @app.route("/api/v1.0/<start>", endpoint="onearg")
@@ -96,20 +111,6 @@ def start_date(start, end):
         return jsonify(list_result)
     except:
         # The fail message will tell the User which dates are available dependin on the min and max date calculated before
-        return(f"""The date you entered is not found.
-        Please try with dates from {min_date} to {max_date}""")
-
-
-def star_end(start,end):
-    min_date = session.query(func.min(Measurement.tobs).filter(and_(Measurement.date >= start, Measurement.date <= end))).all()[0][0]
-    max_date = session.query(func.max(Measurement.tobs).filter(and_(Measurement.date >= start, Measurement.date <= end))).all()[0][0]
-    avg_date = session.query(func.avg(Measurement.tobs).filter(and_(Measurement.date >= start, Measurement.date <= end))).all()[0][0]
-    range_result = [min_date,max_date,avg_date]
-
-    try:
-        min_date + 1
-        return jsonify(range_result)
-    except:
         return(f"""The date you entered is not found.
         Please try with dates from {min_date} to {max_date}""")
 
